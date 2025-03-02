07df7b4f41fd0c5e04ca143729eaf739b059eca8
John Paul <supp@0day.is>
2025-03-02 13:59:35 +0900

    feat: integrate BluetoothCLI into Cli struct for enhanced command handling

--- Sources/cli.swift
+++ Sources/cli.swift
@@ -1,18 +1,23 @@
-
+import Foundation
 struct Cli {
     let arguments: [String]
+    let ble = BluetoothCLI()
 
     init(arguments: [String] = CommandLine.arguments) {
         self.arguments = arguments
     }
 
-    func view_menu(title: String, inputLabel: String, child: String) {
+    func view_prompt(title: String, inputLabel: String, child: String) {
         print("\n--- \(title) ---")
         print(child)
         print("\(inputLabel)> ", terminator: "")
     }
 
     func run() {
-        print("Hello, world!")
+        ble.run()
+        ble.onDone = {
+            print(ble.iqosIlumaI)
+        }
+        RunLoop.main.run()
     }
 }
