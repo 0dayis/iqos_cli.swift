d473fbb35c38c914b3200922ac6e0c035a1415e5
John Paul <supp@0day.is>
2025-03-03 12:16:57 +0900

    feat: enhance CLIPrompt with Header struct and update Menu class for improved command listing

--- Sources/prompt.swift
+++ Sources/prompt.swift
@@ -4,18 +4,30 @@ protocol CLIPrompt {
     func view(param: PromptParam)
 }
 
+
+struct Header {
+    var title: String
+    var menu: Menu
+    var view: () -> Void
+
+    func view() {
+        print("\n--- \(title) ---")
+    }
+}
+
 struct PromptParam {
     var title: String
     var promptLabel: String
     var child: [String]
+    var quit
 }
 
-class Prompt: NSObject, CLIPrompt {
-    func view(param: PromptParam) {
-        print("\n--- \(param.title) ---")
+class Menu: NSObject, CLIPrompt {
+    func listView(param: PromptParam) {
         for i in 0..<param.child.count {
             print("[\(i+1)]: \(param.child[i])")
         }
+        print("[0]: Quit")
         print("\(param.promptLabel)> ", terminator: "")
         let command: String? = readLine()
 
