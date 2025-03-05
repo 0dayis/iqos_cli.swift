3dc53a59fdbcc3f48fbd8a6499235435d7b52c2c
John Paul <supp@0day.is>
2025-03-05 18:15:45 +0900

    fix: update dependency from SwiftTUI to swift-argument-parser for improved functionality

--- Package.swift
+++ Package.swift
@@ -5,19 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "iqos_cli",
-    platforms: [
-        .macOS(.v11)
+    products: [
+        .executable(name: "iqos_cli", targets: ["iqos_cli"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rensbreur/SwiftTUI.git", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "iqos_cli",
-            dependencies: ["SwiftTUI"]),
-        .testTarget(name: "HCI_Gather"),
-        // .testTarget(name: "TestTUI", dependencies: ["SwiftTUI"], path: "Tests/TestTUI"),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
     ]
 )
