7b6688e7facc7ee06bdca92bcf21f9034fedc4c1
John Paul <supp@0day.is>
2025-03-05 18:16:04 +0900

    refactor: remove commented-out Cli struct and related methods from Console.swift

--- Sources/Console.swift
+++ Sources/Console.swift
@@ -1,29 +1,6 @@
 import Foundation
 import ArgumentParser
 
-// struct Cli {
-//     let arguments: [String]
-//     let ble = BluetoothCLI()
-
-//     init(arguments: [String] = CommandLine.arguments) {
-//         self.arguments = arguments
-//     }
-
-//     func view_prompt(title: String, inputLabel: String, child: String) {
-//         print("\n--- \(title) ---")
-//         print(child)
-//         print("\(inputLabel)> ", terminator: "")
-//     }
-
-//     func run() {
-//         let console = Console()
-//         console.run()
-//         // ble.onDone = {
-//         //     // print(ble.iqosIlumaI)
-//         //     // ble.iqosIlumaI.view()
-//         // }
-//     }
-// }
 extension IQOSCommand {
     struct BatteryCommand: ParsableCommand {
         static let configuration = CommandConfiguration(
