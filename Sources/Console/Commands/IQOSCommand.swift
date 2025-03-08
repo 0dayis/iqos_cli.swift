e6ac7a1eda95490d7b646c765181b16f41b1fcde
John Paul <supp@0day.is>
2025-03-08 19:42:22 +0900

    feat: add battery, lock, unlock, and find my IQOS commands to CLI

--- Sources/Console/Commands/IQOSCommand.swift
+++ Sources/Console/Commands/IQOSCommand.swift
@@ -1,6 +1,80 @@
 import Foundation
 import ArgumentParser
 
+extension IQOSCommand {
+    struct BatteryCommand: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "battery",
+            abstract: "Show or manipulate battery information"
+        )
+
+        // @Argument(help: "The battery level")
+        // var level: Int?
+        
+        // @Flag(name: .shortAndLong, help: "Show detailed battery info")
+        // var detailed: Bool = false
+        
+        mutating func run(iqos: IQOS) {
+            print("Battery: \(iqos.batteryStatus())%")
+        }
+    }
+
+    struct Lock: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "lock",
+            abstract: "Lock the IQOS"
+        )
+
+        mutating func run(iqos: IQOS) {
+            iqos.lock()
+            print("Locked the IQOS")
+        }
+    }
+
+    struct UnLock: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "unlock",
+            abstract: "Unlock the IQOS"
+        )
+
+        mutating func run(iqos: IQOS) {
+            iqos.unlock()
+            print("Unlocked the IQOS")
+        }
+    }
+
+    struct FindMyIQOS: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "findmyiqos",
+            abstract: "Find my IQOS"
+        )
+
+        @Argument(help: "Stop finding my IQOS")
+        var stop: String?
+
+        @Option(name: .shortAndLong, help: "Interval in seconds")
+        var interval: Int?
+
+        mutating func run(iqos: IQOS) {
+            if let stop = stop {
+                if stop == "stop" {
+                        iqos.stopViberation()
+                        print("Stop finding my IQOS")
+                }
+            } else {
+                print("Finding my IQOS...")
+                iqos.viberation()
+                if let interval = interval {
+                    sleep(UInt32(interval))
+                    iqos.stopViberation()
+                    print("Stop finding my IQOS")
+                }
+            }
+        }
+    }
+
+}
+
 struct IQOSCommand: ParsableCommand {
     static let configuration = CommandConfiguration(
         commandName: "iqos",
